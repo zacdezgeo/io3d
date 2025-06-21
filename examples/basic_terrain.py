63dbb3f8302cf1155a53f5b0013d23d5a0521026
Zac Deziel <zachary.deziel@gmail.com>
2025-06-21 18:53:29 +0000

    Add examples

---
+++ examples/basic_terrain.py
@@ -0,0 +1,11 @@
+import rasterio
+from io3d import raster_to_mesh, export_ply
+
+# Path to the DEM file
+path = "data/squamish.tif"
+
+with rasterio.open(path) as src:
+    elev = src.read(1).astype('float32')
+
+mesh = raster_to_mesh(elev, None)
+export_ply(mesh, "squamish.ply")
